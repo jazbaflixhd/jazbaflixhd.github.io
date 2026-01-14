bd52318f49b6ec83afc80bd72530b38fa1405a12
jazbaflixhd <jazbaflixhd@gmail.com>
2026-01-14 14:18:13 +0500

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,21 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
+<script>
+firebase.initializeApp({
   apiKey: "AIzaSyDg9nJ36YnxwcSY0CPAIKXnc_9MDltlVRw",
   authDomain: "jazbaflix-push.firebaseapp.com",
   projectId: "jazbaflix-push",
-  storageBucket: "jazbaflix-push.firebasestorage.app",
   messagingSenderId: "294121382918",
-  appId: "1:294121382918:web:c2a12d0485d56b754f0f87",
-  measurementId: "G-XMF993L90S"
-};
+  appId: "1:294121382918:web:c2a12d0485d56b754f0f87"
+});
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const messaging = firebase.messaging();
+
+function requestPermission(){
+Notification.requestPermission().then(permission=>{
+if(permission==="granted"){
+messaging.getToken({vapidKey:"BFGi7vl0HCYRweWv4qGWBIArkTZoOJVun-JFYhcJws7qdtB5QkKo19TRqbcd3Jd6A6oKdC585S-Who1fsBpQAk4"}).then(token=>{
+console.log("TOKEN:",token);
+});
+}
+});
+}
+</script>
